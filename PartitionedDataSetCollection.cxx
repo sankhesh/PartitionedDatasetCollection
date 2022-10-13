82e57ae751a06a355d27557b85c296a951ec0efd
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-10-13 14:16:26 -0400

    Initial commit

---
+++ PartitionedDataSetCollection.cxx
@@ -0,0 +1,12 @@
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkNew.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkPartitionedDataSetCollection.h>
+
+int main(int, char* [])
+{
+  return EXIT_SUCCESS;
+}
